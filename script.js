928e7d0cdeb9a9cd589656f68de982babdf2e9fe
WebDev47621 <webdev47621@gmail.com>
2021-01-07 00:18:21 -0800

    Fix stack section expansion

--- script.js
+++ script.js
@@ -7,12 +7,36 @@ VanillaTilt.init(document.querySelectorAll(".glass-tilt .row .card"), {
 const stackSec = document.querySelector('.three-dim-stack');
 const stackRow = document.querySelector('.three-dim-stack .row');
 
-if (window.innerWidth <= 1568) {
+let width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+if (width <= 1100) {
     stackRow.addEventListener('mouseover', expandSection);
     stackRow.addEventListener('mouseleave', contractSection);
-} else {
+}
+
+if (width > 1100) {
     stackRow.removeEventListener('mouseover', expandSection);
     stackRow.removeEventListener('mouseleave', contractSection);
+}
+
+window.addEventListener('resize', checkBrowserSize);
+
+function checkBrowserSize() {
+    let width = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    
+    if (width <= 1050) {
+        stackRow.addEventListener('mouseover', expandSection);
+        stackRow.addEventListener('mouseleave', contractSection);
+    }
+    
+    if (width > 1050) {
+        stackRow.removeEventListener('mouseover', expandSection);
+        stackRow.removeEventListener('mouseleave', contractSection);
+    }
 }
 // stackRow.addEventListener('mouseover', expandSection);
 // stackRow.addEventListener('mouseleave', contractSection);
